6c88d0d08e90e873bdad91a96c01fb9ff119b2dc
David Pacheco <getwaste037@gmail.com>
2026-08-18 14:11:47 -0600

    Correcion en excepcion global para que tome la estructura perzonalizada al salte un ResourceNotFoundException

--- src/main/java/com/clinicaOdonto/Clinica/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clinicaOdonto/Clinica/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.clinicaOdonto.Clinica.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +11,7 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
         Map<String, Object> body = new HashMap<>();
         body.put("status", HttpStatus.NOT_FOUND.value());
