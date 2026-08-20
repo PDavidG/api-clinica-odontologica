7ab68c7f9576028f8d848ea07ca2fc64fb0e8f03
David Pacheco <getwaste037@gmail.com>
2026-08-19 18:39:12 -0600

    creacion de usuarioMapper

---
+++ src/main/java/com/clinicaOdonto/Clinica/mapper/UsuarioMapper.java
@@ -0,0 +1,33 @@
+package com.clinicaOdonto.Clinica.mapper;
+
+import com.clinicaOdonto.Clinica.domain.Usuario;
+import com.clinicaOdonto.Clinica.dto.UsuarioRequestDto;
+import com.clinicaOdonto.Clinica.dto.UsuarioResponseDto;
+
+public interface UsuarioMapper {
+
+    public static UsuarioResponseDto toDto(Usuario user) {
+
+        if (user == null) {
+            return null;
+        }
+
+        UsuarioResponseDto userDto = new UsuarioResponseDto();
+        userDto.setUsername(user.getUsername());
+        userDto.setRol(user.getRol());
+        return userDto;
+    }
+
+    public static Usuario toEntity(UsuarioRequestDto requestDto) {
+
+        if (requestDto == null) {
+            return null;
+        }
+
+        Usuario user = new Usuario();
+        user.setUsername(requestDto.getUsername());
+        user.setPassword(requestDto.getPassword());
+        user.setRol(requestDto.getRol());
+        return user;
+    }
+}
