afd95cc3e24e915d1b96c30b0b96214bbb588221
David Pacheco <getwaste037@gmail.com>
2026-08-20 18:13:26 -0600

    Se agrega la anotacion Valid a odontologoController.

--- src/main/java/com/clinicaOdonto/Clinica/controller/OdontologoController.java
+++ src/main/java/com/clinicaOdonto/Clinica/controller/OdontologoController.java
@@ -5,6 +5,7 @@ import com.clinicaOdonto.Clinica.dto.OdontologoRequestDto;
 import com.clinicaOdonto.Clinica.dto.OdontologoResponseDto;
 import com.clinicaOdonto.Clinica.mapper.OdontologoMapper;
 import com.clinicaOdonto.Clinica.service.IOdontologoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,14 +38,14 @@ public class OdontologoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<OdontologoResponseDto> crearOdontologo(@RequestBody OdontologoRequestDto requestDto) {
+    public ResponseEntity<OdontologoResponseDto> crearOdontologo(@Valid @RequestBody OdontologoRequestDto requestDto) {
         Odontologo odon = odontologoService.save(requestDto);
         OdontologoResponseDto responseDto = OdontologoMapper.toDto(odon);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OdontologoResponseDto> actualizarOdontologo(@PathVariable Long id, @RequestBody OdontologoRequestDto requestDto) {
+    public ResponseEntity<OdontologoResponseDto> actualizarOdontologo(@PathVariable Long id, @Valid @RequestBody OdontologoRequestDto requestDto) {
         Odontologo updateOdonto = odontologoService.update(id, requestDto);
         return ResponseEntity.ok(OdontologoMapper.toDto(updateOdonto));
     }
