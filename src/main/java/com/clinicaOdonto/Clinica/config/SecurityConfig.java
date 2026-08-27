59b7894be674040ea0388c82ad0d3a411cf63e26
David Pacheco <getwaste037@gmail.com>
2026-08-26 21:42:34 -0600

    Creación de clase SecurityConfig donde se llavara a cabo la gestion de beans (Filterchain, passwordEncoder, authenticationManager)

---
+++ src/main/java/com/clinicaOdonto/Clinica/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.clinicaOdonto.Clinica.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Esto para que desactive las cookies que maneja por default
+                .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/api/v1/auth/**").permitAll();
+                auth.anyRequest().authenticated();
+        })
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//  Sirve para recibir las crendenciales (usuario y constraseña) para decidir si son validos o no
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration authenticationConfiguration)throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
