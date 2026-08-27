ad8334dd2b5946d60ec7baba2ef06368f35cd1c2
David Pacheco <getwaste037@gmail.com>
2026-08-27 14:45:53 -0600

    Creación del metodo para genera el token.

--- src/main/java/com/clinicaOdonto/Clinica/security/jwt/JwtGenerator.java
+++ src/main/java/com/clinicaOdonto/Clinica/security/jwt/JwtGenerator.java
@@ -22,4 +22,19 @@ public class JwtGenerator {
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpiration);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(currentDate)
+                .expiration(expireDate)
+                // Toma la clave secreta y el contenido de jwt y genera una firma digital de
+                // 512 bytes que garantiza Integridad, Autenticidad.
+                .signWith(getSigningKey(), Jwts.SIG.HS512)
+                .compact();
+    }
 }
