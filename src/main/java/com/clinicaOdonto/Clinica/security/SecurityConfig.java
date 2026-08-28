c6c137c94a641e24ec3a33fb4b170171fcd3e431
David Pacheco <getwaste037@gmail.com>
2026-08-28 17:17:17 -0600

    Se agrega anotacion @EnebleMethodSecurity para aplicar reglas de autorizacion

--- src/main/java/com/clinicaOdonto/Clinica/security/SecurityConfig.java
+++ src/main/java/com/clinicaOdonto/Clinica/security/SecurityConfig.java
@@ -5,9 +5,9 @@ import com.clinicaOdonto.Clinica.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
