f0bdcaec5d6f602facda72976071b5c85678aa26
David Pacheco <getwaste037@gmail.com>
2026-08-28 14:21:29 -0600

    Se crea la clase UserDetailsServiceImpl

---
+++ src/main/java/com/clinicaOdonto/Clinica/service/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.clinicaOdonto.Clinica.service;
+
+import com.clinicaOdonto.Clinica.domain.Role;
+import com.clinicaOdonto.Clinica.domain.Usuario;
+import com.clinicaOdonto.Clinica.repository.UsuarioRespository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    private final UsuarioRespository usuarioRespository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario user = usuarioRespository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuario no encontrado con el username: " + username));
+
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
