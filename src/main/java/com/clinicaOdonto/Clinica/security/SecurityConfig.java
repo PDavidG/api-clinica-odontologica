aad01592f88dd3bffca7aded80c08bf75f3bf5f6
David Pacheco <getwaste037@gmail.com>
2026-08-28 10:04:48 -0600

    Se modifica para añadir nuestros metodos creados del jwt(Filter, EntryPoint) en el metodo filterChain

--- src/main/java/com/clinicaOdonto/Clinica/security/SecurityConfig.java
+++ src/main/java/com/clinicaOdonto/Clinica/security/SecurityConfig.java
@@ -1,35 +1,44 @@
 package com.clinicaOdonto.Clinica.security;
 
+import com.clinicaOdonto.Clinica.security.jwt.JwtAuthEntryPoint;
+import com.clinicaOdonto.Clinica.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable) // Esto para que desactive las cookies que maneja por default
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/api/v1/auth/**").permitAll();
                 auth.anyRequest().authenticated();
-        })
-                .httpBasic(Customizer.withDefaults());
-
+        });
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
